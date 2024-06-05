15e9bfd9e9f6966c97e425a0dd23c74ee85443dd
Chris Hung Huynh <38028924+hhuyn019@users.noreply.github.com>
2024-06-05 13:17:58 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Employee {
+import java.util.ArrayList;
+
+public class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
