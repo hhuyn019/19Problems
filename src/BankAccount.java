cf69eb14852dd58c9ed9318374f9581375ee60f5
Chris Hung Huynh <38028924+hhuyn019@users.noreply.github.com>
2024-06-05 13:18:09 -0700

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public BankAccount() {
+    }
+
+    void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    void withdraw(double amount) {
+        if(amount > balance) {
+            System.out.println("Insufficient funds");
+            return;
+        }
+        this.balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
